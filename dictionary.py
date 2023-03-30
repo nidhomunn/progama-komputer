ff39702223f6535c558790e21c629aaa4c2169c2
Nidhomun <fitrinidhomun@gmail.com>
2023-03-30 12:07:11 +0700

    pesan 1

---
+++ dictionary.py
@@ -0,0 +1,6 @@
+string = "ikan bakar gosong pait banget"
+
+print (string[0:4])
+print (string[10:2])
+print (string[0:29:])
+print (string[29:0:-1])
---
+++ minggu ke 2..py
@@ -0,0 +1,15 @@
+celcius = int(input("masukkan nilai celcius :"))
+
+fahrenheit = (9/5)*celcius+32
+print (f"nilai {celcius} C ke F adalah : {fahrenheit}")
+
+print ("=============")
+
+#reamur ke fahrenheit
+
+reamur = int(input("masukkan nilai reamur :"))
+fahrenheit = (9/4)*reamur +32
+print (f"nilai {reamur} reamur ke fahrenheit adalah : {fahrenheit}")
+
+
+ 
